670498e223f2653590c8eed6ac8ff18f3c294fbb
mamgo <420923119@qq.com>
2019-06-21 10:57:02 +0800

    init data

---
+++ src/main/java/com/xlx/majiang/MajiangApplication.java
@@ -0,0 +1,13 @@
+package com.xlx.majiang;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajiangApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MajiangApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/xlx/majiang/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.xlx.majiang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 测试
+ * @author xielx
+ * @date 2019/6/21
+ **/
+@Controller
+public class HelloController {
+
+  /*
+   *
+   * @param name
+   * @return
+   */
+  @GetMapping("/hello")
+  public String index(@RequestParam(name = "username") String name, Model model){
+    model.addAttribute("name",name);
+    return "hello" ;
+  }
+
+}
