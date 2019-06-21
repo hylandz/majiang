3c2a3be0b29814aa08697cfd42c5b9932f4f4fbb
majiang <xielinx163@163.com>
2019-06-21 14:41:37 +0800

    add static file

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 + 在GitHub上创建一个仓库,名称:majiang
 + 使用git命令本地仓库与远程同步
 
+## 使用技术
++ spring-boot
++ mybatis
++ h2,mysql
++ okhttp3
++ lombok
++ flywaydb
++ heroku
+
 ## 关于github仓库的注意事项
 + 需要为本地仓库添加一个远程仓库的别名
 + 可以为当前项目设置自己用户名与邮箱,好与其他项目区分
--- src/main/java/com/xlx/majiang/controller/HelloController.java
+++ src/main/java/com/xlx/majiang/controller/HelloController.java
@@ -21,7 +21,7 @@ public class HelloController {
   @GetMapping("/hello")
   public String index(@RequestParam(name = "username") String name, Model model){
     model.addAttribute("name",name);
-    return "hello" ;
+    return "index";
   }
 
 }
