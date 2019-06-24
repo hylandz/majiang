c2cc1765ee98f8f9fcbe152b895d51b4b94ff09f
majiang <xielinx163@163.com>
2019-06-25 02:52:43 +0800

    finally,i can get some rest

---
+++ src/main/java/com/xlx/majiang/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package com.xlx.majiang.mapper;
+
+import com.xlx.majiang.model.User;
+import com.xlx.majiang.model.UserExample;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExampleWithRowbounds(UserExample example, RowBounds rowBounds);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
