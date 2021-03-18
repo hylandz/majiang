c1a267cfd779305cfe1277b0ac89debd196fed5d
perfectxlx <perfectxlx@gmail.com>
2021-03-18 14:15:13 +0800

    加入aop的登录日志显示

--- src/main/java/com/xlx/majiang/aop/LogAspect.java
+++ src/main/java/com/xlx/majiang/aop/LogAspect.java
@@ -1,9 +1,14 @@
 package com.xlx.majiang.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 /**
@@ -11,8 +16,8 @@ import java.util.Arrays;
  *
  * @author xielx on 2019/8/20
  */
-//@Component
-//@Aspect
+@Component
+@Aspect
 public class LogAspect {
     
     
@@ -23,8 +28,8 @@ public class LogAspect {
      * 切入点
      * execution(方法修饰符(可选) 返回类型 方法名 参数 异常模式(可选))
      */
-    //@Pointcut("execution(public * com.xlx.majiang.system.controller.*.*(..))")
-    public void LogAspect() {}
+    @Pointcut("execution(public * com.xlx.majiang.system.controller.LoginController.doLogin())")
+    public void LoginAspect() {}
     
     /**
      * ************************
@@ -32,7 +37,11 @@ public class LogAspect {
      * ************************
      */
     
-    //@Before("LogAspect()")
+    @After("LoginAspect()")
+    public void doLoginLog(JoinPoint joinPoint) {
+    	logger.info("您在{},执行登录操作~~", LocalDateTime.now());
+	    
+    }
     public void doBefore(JoinPoint joinPoint) {
     	logger.info("<-----------前置通知,方法执行前执行--------------->");
         //前置通知,方法前执行
